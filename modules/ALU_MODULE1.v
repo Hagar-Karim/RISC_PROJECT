9cb4277077cad00559aaa8627646dd7a7e79018c
hagora <hagar.hassan04@eng-st.cu.edu.eg>
2025-06-24 13:03:24 +0300

    Added AlU module

---
+++ modules/ALU_MODULE1.v
@@ -0,0 +1,26 @@
+module alu_module 
+(
+   input wire [2:0] AluControl ,
+   input wire [31:0] A, B,
+   output reg ZeroFlag, SignFlag,
+   output reg [31:0] ALUResult 
+
+);
+
+always@(*)
+begin
+case(AluControl)
+
+3'b000 :  ALUResult = A + B;
+3'b001 :  ALUResult = A << B;
+3'b010 :  ALUResult = A - B;
+3'b100 :  ALUResult = A ^ B;
+3'b101 :  ALUResult = A >>> B; //arethmetic shift
+3'b110 :  ALUResult = A | B;
+3'b111 :  ALUResult= A & B;
+default : ALUResult= 32'b0;
+endcase
+ZeroFlag = (ALUResult==0);
+SignFlag =  ALUResult[31];
+end //always end
+endmodule
